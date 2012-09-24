2721751fbfa1bc27f5865f88a0c927b554a323f3
Vincent Zhang <young79227@gmail.com>
2012-09-24 16:21:59 +0800

    add Equipment entity

---
+++ Solutions/BeautySalonManagement.Domain/Articles/Equipment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using SharpArch.Domain.DomainModel;
+using SharpArch.NHibernate.NHibernateValidator;
+
+namespace BeautySalonManagement.Domain.Articles
+{
+		[HasUniqueDomainSignature(ErrorMessage = "名称已经存在")]
+	public class Equipment:Entity
+	{
+		[DomainSignature]
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ Solutions/BeautySalonManagement.Infrastructure/NHibernateMaps/CustomMap/EquipmentMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BeautySalonManagement.Domain.Articles;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+
+namespace BeautySalonManagement.Infrastructure.NHibernateMaps.CustomMap
+{
+	public class EquipmentMap : IAutoMappingOverride<Equipment>
+	{
+		public void Override(AutoMapping<Equipment> mapping)
+		{
+			mapping.Map(x => x.Name).Not.Nullable().UniqueKey("EquipmentName");
+		}
+	}
+}
